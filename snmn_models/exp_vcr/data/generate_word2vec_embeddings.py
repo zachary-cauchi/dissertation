50cc3306103198e372685c38918022bfc3250c12
Zachary Cauchi <zach.cauchi@hotmail.com>
2023-08-25 07:31:27 +0000

    feat: add support for higher vector dims

--- snmn_models/exp_vcr/data/generate_word2vec_embeddings.py
+++ snmn_models/exp_vcr/data/generate_word2vec_embeddings.py
@@ -5,14 +5,14 @@ from gensim.models import word2vec
 parser = argparse.ArgumentParser(prog='generate_word2vec_embeddings.py', description='Generate Word2Vec embeddings for the VCR corpus.')
 parser.add_argument('--file_path', type=str, default='./corpus_vcr.txt')
 parser.add_argument('--model_type', type=str, choices=['cbow', 'sg'], default='cbow')
+parser.add_argument('--vector_size', type=int, default=300)
 parser.add_argument('--vocab_file', type=str, default='./vocabulary_vcr.txt')
-parser.add_argument('--output_file', type=str, default='./vocabulary_vcr_word2vec.txt')
+parser.add_argument('--output_file', type=str, default='')
 parser.add_argument('--output_npy_file', type=str, default='./vocabulary_vcr_word2vec.npy')
 
 args = parser.parse_args()
 
-
-model = word2vec.Word2Vec(corpus_file=args.file_path, min_count=1, vector_size=300, sg = 1 if args.model_type == 'sg' else 0)
+model = word2vec.Word2Vec(corpus_file=args.file_path, min_count=1, vector_size=args.vector_size, sg = 1 if args.model_type == 'sg' else 0)
 
 with open(args.vocab_file, 'r') as vocab_file:
     vocab_words = [ line.strip().split(' ')[0] for line in vocab_file ]
@@ -25,7 +25,8 @@ assert np.array_equal(vectors_array[5], model.wv[vocab_words[5]]), 'numpy array 
 
 np.save(args.output_npy_file, vectors_array, allow_pickle=True)
 
-with open(args.output_file, 'w') as out_txt:
-    for vector in vectors_array:
-        vector_str = ' '.join(map(str, vector))
-        out_txt.write(vector_str + '\n')
+if len(args.output_file) == 0:
+    with open(args.output_file, 'w') as out_txt:
+        for vector in vectors_array:
+            vector_str = ' '.join(map(str, vector))
+            out_txt.write(vector_str + '\n')
