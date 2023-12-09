ecd40e220e1e57b54d6adc1d4aff6d7803ce35ef
Zachary Cauchi <zach.cauchi@hotmail.com>
2023-12-09 10:19:44 +0000

    feat: add prediction metrics counter to script

--- snmn_models/exp_vcr/match_correct_predictions.py
+++ snmn_models/exp_vcr/match_correct_predictions.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+from collections import Counter
 
 parser = argparse.ArgumentParser(prog='matchCorrectPredictions')
 parser.add_argument('--task-type', choices=['qa', 'qar'], default='qar')
@@ -22,6 +23,7 @@ load_rationale = args.task_type == 'qar'
 correct_answers = []
 correct_rationales = []
 correct_predictions = []
+metrics = Counter()
 
 for i, (pred, truth) in enumerate(zip(predictions, truth_answers)):
     guessed_answer = False
@@ -29,20 +31,33 @@ for i, (pred, truth) in enumerate(zip(predictions, truth_answers)):
 
     if pred['answer'] == truth['answer']:
         correct_answers.append(pred)
+        metrics['correct_answer'] += 1
         guessed_answer = True
 
     if load_rationale and pred['rationale'] == truth['rationale']:
         correct_rationales.append(pred)
+        metrics['correct_rationale'] += 1
         guessed_rationale = True
 
     if guessed_answer and (not load_rationale or guessed_rationale):
         correct_predictions.append(pred)
+        metrics['correct_prediction'] += 1
+    elif not guessed_answer and load_rationale and guessed_rationale:
+        metrics['incorrect_answer_correct_rationale'] += 1
+    elif guessed_answer and load_rationale and not guessed_rationale:
+        metrics['correct_answer_incorrect_rationale'] += 1
+    elif (not guessed_answer and not load_rationale) or (not guessed_answer and load_rationale and not guessed_rationale):
+        metrics['wrong_prediction'] += 1
 
 total_predictions = len(predictions)
 total_correct_predictions = len(correct_predictions)
 
 print(f'Evaluated {total_predictions} predictions.')
-print(f'Correct answers: {len(correct_answers)}, correct rationales: {len(correct_rationales)}')
+print(f'Correct answers: {metrics["correct_answer"]}, correct rationales: {metrics["correct_rationale"]}')
 print(f'Correct predictions: {total_correct_predictions} in {"QA" if not load_rationale else "QAR"} mode ({(total_correct_predictions / total_predictions) * 100:.2f}%).')
-if load_rationale:
-    print(f'Correct answers but incorrect predictions')
+
+if 'correct_answer_incorrect_rationale' in metrics:
+    print(f'Correct answers but incorrect predictions: {metrics["correct_answer_incorrect_rationale"]} ({(metrics["correct_answer_incorrect_rationale"] / total_predictions) * 100:.2f}%)')
+if 'incorrect_answer_correct_rationale' in metrics:
+    print(f'Incorrect answers but correct predictions: {metrics["incorrect_answer_correct_rationale"]} ({(metrics["incorrect_answer_correct_rationale"] / total_predictions) * 100:.2f}%)')
+
